ac1fc9816fd87bf91939032c736ef77bbb19b9d8
csabika98 <sallcsa.csaba8@gmail.com>
2024-06-18 13:27:34 +0200

    NOTICKET adding templating logic, adding way to add add custom .html and render them

--- src/controller/StaticController.hpp
+++ src/controller/StaticController.hpp
@@ -1,4 +1,4 @@
-
+// src/controller/StaticController.hpp
 #ifndef CRUD_STATICCONTROLLER_HPP
 #define CRUD_STATICCONTROLLER_HPP
 
@@ -6,40 +6,41 @@
 #include "oatpp/json/ObjectMapper.hpp"
 #include "oatpp/macro/codegen.hpp"
 #include "oatpp/macro/component.hpp"
+#include "service/TemplateService.hpp"
+#include <unordered_map>
 
 #include OATPP_CODEGEN_BEGIN(ApiController) //<- Begin Codegen
 
 class StaticController : public oatpp::web::server::api::ApiController {
 public:
-  explicit StaticController(const std::shared_ptr<oatpp::web::mime::ContentMappers>& apiContentMappers)
-    : oatpp::web::server::api::ApiController(apiContentMappers)
-  {}
+    explicit StaticController(const std::shared_ptr<oatpp::web::mime::ContentMappers>& apiContentMappers)
+            : oatpp::web::server::api::ApiController(apiContentMappers)
+    {}
 public:
 
-  static std::shared_ptr<StaticController> createShared(
-    OATPP_COMPONENT(std::shared_ptr<oatpp::web::mime::ContentMappers>, apiContentMappers) // Inject ContentMappers
-  ){
-    return std::make_shared<StaticController>(apiContentMappers);
-  }
+    static std::shared_ptr<StaticController> createShared(
+            OATPP_COMPONENT(std::shared_ptr<oatpp::web::mime::ContentMappers>, apiContentMappers) // Inject ContentMappers
+    ){
+        return std::make_shared<StaticController>(apiContentMappers);
+    }
 
-  ENDPOINT("GET", "/", root) {
-    const char* html =
-      "<html lang='en'>"
-      "  <head>"
-      "    <meta charset=utf-8/>"
-      "  </head>"
-      "  <body>"
-      "    <p>Hello CRUD example project!</p>"
-      "    <a href='swagger/ui'>Checkout Swagger-UI page</a>"
-      "  </body>"
-      "</html>";
-    auto response = createResponse(Status::CODE_200, html);
-    response->putHeader(Header::CONTENT_TYPE, "text/html");
-    return response;
-  }
+    ENDPOINT("GET", "/", root) {
+        try {
+            std::string html = TemplateService::loadTemplate("example.html");
+            std::unordered_map<std::string, std::string> values = {{"value", "Hello from backend"}};
+            std::string renderedHtml = TemplateService::renderTemplate(html, values);
+            auto response = createResponse(Status::CODE_200, renderedHtml.c_str());
+            response->putHeader(Header::CONTENT_TYPE, "text/html");
+            return response;
+        } catch (const std::exception& e) {
+            return createResponse(Status::CODE_500, e.what());
+        }
+    }
 
 };
 
 #include OATPP_CODEGEN_END(ApiController) //<- End Codegen
 
 #endif //CRUD_STATICCONTROLLER_HPP
+
+
