5991ae8015aa5c0e806af0f93502587fba177f29
csaba.sallai <csaba.sallai@innoveo.com>
2024-06-19 22:53:06 +0200

    NOTICKET Making changes in the Powershell script, creating documentation how to install the app on Windows p2

--- utility/install.ps1
+++ utility/install.ps1
@@ -1,46 +1,46 @@
-# Define the script parameters
+
 Param(
     [string]$BUILD_TYPE = "Debug"
 )
 
-# Path to the compilers and make tool
+
 $mingwBinPath = "C:\msys64\mingw64\bin"
 #$installPrefix = "C:\Users\CsabaSallai\oatpp-install"  # Set your user-space installation prefix
 
-# Clean up old temporary directory
+
 Remove-Item -Path "tmp" -Recurse -Force -ErrorAction SilentlyContinue
 
-# Create a new temporary directory
+
 New-Item -Path "tmp" -ItemType Directory | Out-Null
 Set-Location -Path "tmp"
 
 ##########################################################
-## Function to install oatpp module
+
 function Install-Module {
     param(
         [string]$BuildType,
         [string]$ModuleName
     )
 
-    # Get the number of processors
+
     $NPROC = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
 
-    # Properly use $null on the left side of the equality
+
     if ($null -eq $NPROC -or 0 -eq $NPROC) {
         $NPROC = 1
     }
 
     Write-Host "`n`nINSTALLING MODULE '$ModuleName' ($BuildType) using $NPROC threads...`n`n"
 
-    # Clone the repository
+
     git clone --depth=1 "https://github.com/oatpp/$ModuleName"
 
-    # Build the module
+
     Set-Location -Path "$ModuleName"
     New-Item -Path "build" -ItemType Directory | Out-Null
     Set-Location -Path "build"
 
-    # Set the C and C++ compilers and add the path to the environment
+
     $env:PATH = "$mingwBinPath;$env:PATH"
     $makeProgram = "$mingwBinPath\mingw32-make.exe"
 
@@ -66,16 +66,16 @@ function Install-Module {
 }
 
 ##########################################################
-# Invoke the install function for each module
+
 Install-Module -BuildType $BUILD_TYPE -ModuleName "oatpp"
 
-# Set CMAKE_PREFIX_PATH for subsequent modules
+
 $env:CMAKE_PREFIX_PATH = "$installPrefix;$env:CMAKE_PREFIX_PATH"
 
 Install-Module -BuildType $BUILD_TYPE -ModuleName "oatpp-swagger"
 Install-Module -BuildType $BUILD_TYPE -ModuleName "oatpp-sqlite"
 
-# Cleanup
+
 Set-Location -Path ".."
 Remove-Item -Path "tmp" -Recurse -Force
 
